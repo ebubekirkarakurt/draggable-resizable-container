b8b243d7f6dfd9a3af4300022b15f2d7e0e632bf
ebubekirkarakurt <ebukar75@gmail.com>
2025-07-28 19:06:17 +0300

    small bug fixed

--- docs/src/components/SimpleUsage.tsx
+++ docs/src/components/SimpleUsage.tsx
@@ -9,12 +9,12 @@ export default function SimpleUsage() {
     <div className="main" id="SimpleUsage"> 
         <Title title="🔧 Simple Usage" />
         <Description  description="The following code excerpt demonstrates a basic usage example:" />
-         <div id="SimpleUsageReact" >
+         <div id="#SimpleUsageReact" >
             <Description  description="> React:"/>
         </div>
         
         <CodeBlock code={reactCode} /> 
-        <div id="SimpleUsageVanilla" >
+        <div id="#SimpleUsageVanilla" >
             <Description  description="> Vanilla.Js:" />
         </div>
 
