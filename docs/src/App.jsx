c781e4df7a927b8533c5579022e0eb611a14290f
ebubekirkarakurt <ebukar75@gmail.com>
2025-07-28 14:51:48 +0300

    feat: header added

--- docs/src/App.jsx
+++ docs/src/App.jsx
@@ -1,14 +1,12 @@
 import { useState } from 'react'
 import './App.css'
+import Header from './components/Header'
 
 function App() {
 
   return (
     <>
-      <p>
-        Hey
-      </p>
-      
+      <Header/>
     </>
   )
 }
