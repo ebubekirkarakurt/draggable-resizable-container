6017966dd3c596ad516cd45bf54cbbfd8e8d4b86
ebubekirkarakurt <ebukar75@gmail.com>
2025-08-06 16:36:59 +0300

    chore: ContainerItem data added

--- src/react/components/DraggableResizableAlarmContainerProps.ts
+++ src/react/components/DraggableResizableAlarmContainerProps.ts
@@ -1,14 +1,44 @@
-export interface AlarmItem {
+export interface ContainerItem {
   id: number | string;
-  title: string;
+  label: string;
   alarmStatus: number;
 }
 
 export interface DraggableResizableAlarmContainerProps {
-  data: AlarmItem[] | (() => Promise<AlarmItem[]>);
+  data: ContainerItem[] | (() => Promise<ContainerItem[]>);
   alarmAudioId?: string;
   pollingInterval?: number;
   containerClassName?: string;
   onBoxClick?: (id: string | number) => void;
   soundSrc?: string;
 }
+
+
+ {
+            id: "container-1",
+            label: "Motor",
+            buttons: [
+              {
+                id: "btn-1-1",
+                label: "İmdaat!!",
+                width: 50,
+                currentStage: 1,
+                stages: [
+                  { color: "green" },
+                  { color: "red", blinked: true },
+                  { color: "yellow", blinked: true }
+                ]
+              },
+              {
+                id: "btn-1-2",
+                label: "Help ME!!",
+                width: 50,
+                currentStage: 0,
+                stages: [
+                  { color: "green" },
+                  { color: "red", blinked: true },
+                  { color: "yellow", blinked: true }
+                ]
+              }
+            ]
+          },
