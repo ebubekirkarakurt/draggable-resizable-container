4c9da6dcfe0715977becb257dfa7290a56b9351d
ebubekirkarakurt <ebukar75@gmail.com>
2025-08-06 16:51:44 +0300

    fix: data object updated

--- src/react/components/DraggableResizableAlarmContainerProps.ts
+++ src/react/components/DraggableResizableAlarmContainerProps.ts
@@ -1,44 +1,26 @@
 export interface ContainerItem {
   id: number | string;
   label: string;
-  alarmStatus: number;
+  buttons?: {
+    id: string;
+    label: string;
+    width: number;
+    currentStage: number;
+    stages: {
+      color: string;
+      blinked?: boolean;
+    }[];
+  }[];
 }
 
-export interface DraggableResizableAlarmContainerProps {
+
+export interface DraggableResizableContainerProps {
   data: ContainerItem[] | (() => Promise<ContainerItem[]>);
-  alarmAudioId?: string;
   pollingInterval?: number;
   containerClassName?: string;
-  onBoxClick?: (id: string | number) => void;
-  soundSrc?: string;
+  onButtonStageChanged?: (event: {
+    containerId: string | number;
+    buttonId: string | number;
+    stageIndex: number;
+  }) => void;
 }
-
-
- {
-            id: "container-1",
-            label: "Motor",
-            buttons: [
-              {
-                id: "btn-1-1",
-                label: "İmdaat!!",
-                width: 50,
-                currentStage: 1,
-                stages: [
-                  { color: "green" },
-                  { color: "red", blinked: true },
-                  { color: "yellow", blinked: true }
-                ]
-              },
-              {
-                id: "btn-1-2",
-                label: "Help ME!!",
-                width: 50,
-                currentStage: 0,
-                stages: [
-                  { color: "green" },
-                  { color: "red", blinked: true },
-                  { color: "yellow", blinked: true }
-                ]
-              }
-            ]
-          },
