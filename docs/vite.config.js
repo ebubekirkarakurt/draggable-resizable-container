2117cfc33d5caf6ff9df50d07e8d576d009fc4ed
ebubekirkarakurt <ebukar75@gmail.com>
2025-08-06 18:17:25 +0300

    vite config updated

--- docs/vite.config.js
+++ docs/vite.config.js
@@ -1,13 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
 
 export default defineConfig({
-  root: 'docs',
   plugins: [react()],
-  resolve: {
-    alias: {
-      '/src': path.resolve(__dirname, './src'),
-    },
-  },
 })
