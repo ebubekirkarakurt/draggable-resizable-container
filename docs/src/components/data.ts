dc1b2de2a57cecdfe427e115486e69bc0ddf02f0
ebubekirkarakurt <ebukar75@gmail.com>
2025-08-07 13:48:48 +0300

    feat: new feature added into Readme

--- docs/src/components/data.ts
+++ docs/src/components/data.ts
@@ -1,36 +1,46 @@
 export const data = `const data = {
-        containers: [
+  containers: [
+    {
+      id: "container-1",
+      label: "Engine",
+      labelStyle: {
+        backgroundColor: "#f0f0f0",  
+        border: "1px solid #ccc",     
+        borderRadius: "8px",       
+        padding: "6px 12px",         
+        textStyle: {
+          color: "#d32f2f",           
+          fontSize: "18px",           
+          fontWeight: "bold",        
+        }
+      },
+      buttons: [
         {
-          id: "container-1",
-          label: "Engine",
-          buttons: [
-            {
-              id: "btn-1-1",
-              label: "Overheat Alert",
-              width: 40,
-              currentStage: 1,
-              stages: [
-                { color: "green" },
-                { color: "red", blinked: true },
-                { color: "yellow", blinked: true }
-              ]
-            },
-            {
-              id: "btn-1-2",
-              label: "Maintenance Needed",
-              width: 60,
-              currentStage: 0,
-              stages: [
-                { color: "green" },
-                { color: "red", blinked: true },
-                { color: "yellow", blinked: true }
-              ]
-            }
+          id: "btn-1-1",
+          label: "Overheat Alert",
+          width: 40,
+          currentStage: 1,
+          stages: [
+            { color: "green" },
+            { color: "red", blinked: true },
+            { color: "yellow", blinked: true }
           ]
         },
-      ...
-    ];
-  }
+        {
+          id: "btn-1-2",
+          label: "Maintenance Needed",
+          width: 60,
+          currentStage: 0,
+          stages: [
+            { color: "green" },
+            { color: "red", blinked: true },
+            { color: "yellow", blinked: true }
+          ]
+        }
+      ]
+    }
+  ]
+};
 `
 
 export const reactCode = `import DraggableResizableContainer from 'draggable-resizable-container';
